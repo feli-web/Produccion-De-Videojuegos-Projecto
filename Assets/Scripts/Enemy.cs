b3f2960d1a79ee0a7a02d97bb06a8d9cb2eb4997
feli-web <felipefunes2002@gmail.com>
2024-09-30 22:34:45 -0300

    Sistema de enemigo y jugador parcialmente completados

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,7 +18,7 @@ public class Enemy : MonoBehaviour
         enemyLifeSlider = GameObject.Find("EnemyLifeSlider").GetComponent<Slider>();
         enemyLifeSlider.minValue = 0;
         enemyLifeSlider.maxValue = enemyTime;
-        StartCoroutine(Pattern());
+        StartCoroutine(Patern());
     }
 
     void Update()
@@ -30,7 +30,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public IEnumerator Pattern()
+    public IEnumerator Patern()
     {
         var a = Instantiate(bc, this.transform.position, Quaternion.Euler(0, 0, 0));
         var b = Instantiate(bc, this.transform.position, Quaternion.Euler(0, 0, 0));
@@ -41,40 +41,51 @@ public class Enemy : MonoBehaviour
         a.transform.SetParent(this.transform);
         b.transform.SetParent(this.transform);
 
+        if (timeStages.Length < 2)
+        {
+            Debug.LogError("timeStages array does not have enough values!");
+            yield break;
+        }
+
         while (enemyTime > 0)
         {
-            if (enemyTime > timeStages[0])
+            if (enemyTime > timeStages[0])  // Stage 1
             {
-                // First stage logic
                 aController.spawnerType = BulletsController.SpawnerType.Spin;
                 aController._bulletSpeed = 3;
                 aController._bulletSpinDirection = -1;
-                aController.firingRate = 0.2f;
+                aController.firingRate = 0.5f;
 
                 bController.spawnerType = BulletsController.SpawnerType.Spin;
                 bController._bulletSpeed = 6;
                 bController._bulletSpinDirection = -1;
-                bController.firingRate = 0.2f;
-
-                yield return null;
+                bController.firingRate = 0.5f;
             }
-            else if (enemyTime > timeStages[1] && enemyTime < timeStages[0])
+            else if (enemyTime > timeStages[1] && enemyTime <= timeStages[0])  // Stage 2
             {
                 aController._bulletSpinDirection = 1;
                 bController._bulletSpinDirection = 1;
-
-                yield return null;
+                aController._bulletSpeed = 5;  // Modify the bullet behavior for this stage
+                bController._bulletSpeed = 7;
             }
-        }
+            else if (enemyTime > timeStages[2] && enemyTime <= timeStages[1])  // Stage 3
+            {
+                aController.spawnerType = BulletsController.SpawnerType.Straight;  // Change to a different pattern
+                bController.spawnerType = BulletsController.SpawnerType.Spin;
+                aController._bulletSpeed = 4;
+                bController._bulletSpeed = 6;
+                aController.firingRate = 0.3f;  // Faster firing rate
+                bController.firingRate = 0.3f;
+            }
 
-  
+            yield return null;
+        }
         EndPattern();
     }
 
+
     void EndPattern()
     {
-     
-        Debug.Log("Enemy defeated or pattern completed.");
         Destroy(gameObject); 
     }
 
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Vector2 _rotationPoint = Vector2.zero;
-    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _initialSpeed = 5f; 
+    [SerializeField] private float _maxSpeed = 15f;    
+    [SerializeField] private float _speedIncrement = 0.1f; 
+    private float _currentSpeed;
     private int _rotationDirection = 1;
+
+    void Start()
+    {
+        _currentSpeed = _initialSpeed;
+    }
 
     void FixedUpdate()
     {
-        float angle = _speed * _rotationDirection;
+        float angle = _currentSpeed * _rotationDirection;
         transform.RotateAround(_rotationPoint, Vector3.forward, angle);
+
+        if (_currentSpeed < _maxSpeed)
+        {
+            _currentSpeed += _speedIncrement * Time.fixedDeltaTime;
+        }
     }
 
     public void ChangeOrientation()
     {
         _rotationDirection *= -1;
+        _currentSpeed = _initialSpeed; 
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
